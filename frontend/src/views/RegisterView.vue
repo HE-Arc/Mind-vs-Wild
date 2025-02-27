74a3ae8d91fb4532aa04bc18f2b4dcca4283baf9
Jaton David <david.jaton@he-arc.ch>
2025-02-27 16:10:58 +0100

    add credential to register

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -5,12 +5,12 @@
         <div class="flex flex-column items-center ">
           <q-img src="/logo.png" class="q-mb-md q-mx-auto logo-img" />
           <q-form @submit="register" @reset="reset" class="q-gutter-md full-width q-form">
-            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="email" label="Adresse email*" class="full-width"
-              required outlined />
-            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="username" label="Nom d'utilisateur*" class="full-width"
-              required outlined />
-            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="password" label="Mot de passe*" type="password" required
-              outlined class="full-width" />
+            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="email"
+              label="Adresse email*" class="full-width" required outlined />
+            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="username"
+              label="Nom d'utilisateur*" class="full-width" required outlined />
+            <q-input rounded standout bg-color="white" color="black" input-style="color: black;" v-model="password"
+              label="Mot de passe*" type="password" required outlined class="full-width" />
             <q-btn rounded label="S'inscrire" type="submit" class="full-width btn" />
           </q-form>
         </div>
@@ -42,14 +42,16 @@ export default {
           username: username.value,
           password: password.value,
           email: email.value,
-        })
+        }, {
+          withCredentials: true
+        });
         router.push('/login');
       } catch (error) {
-        errorMessage.value = error.response?.data?.error || "Unknown error"
+        errorMessage.value = error.response?.data?.error || "Unknown error";
       }
     }
 
-    return {email, username, password, register, errorMessage }
+    return { email, username, password, register, errorMessage }
   }
 }
 </script>
@@ -68,13 +70,12 @@ export default {
 }
 
 .q-form {
-  max-width: 800px; 
-  width: 100%; 
+  max-width: 800px;
+  width: 100%;
 }
 
 .q-input {
-  font-size: 1.2rem; 
-  min-height: 56px; 
+  font-size: 1.2rem;
+  min-height: 56px;
 }
-
 </style>
