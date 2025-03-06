2b99241a1e4ae1dd632b5f272c103ca7f9099755
Magnin Nicolas <nicolas.magnin@he-arc.ch>
2025-03-06 06:46:13 +0100

    Add room creation from group

---
+++ api/mindvswild/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Group, GroupUser, GroupInvitation, Room, RoomUser, Question
+from django.contrib.auth.models import User  # Utilisation du modèle User de Django
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class GroupMemberSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = GroupUser
+        fields = ['user', 'is_admin', 'joined_at']
+
+class GroupSerializer(serializers.ModelSerializer):
+    members = GroupMemberSerializer(source='memberships', many=True, read_only=True)
+    created_by = serializers.StringRelatedField()
+
+    class Meta:
+        model = Group
+        fields = ['id', 'name', 'description', 'created_by', 'created_at', 'members']
+
+class RoomParticipantSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = RoomUser
+        fields = ['user', 'joined_at']
+
+class RoomSerializer(serializers.ModelSerializer):
+    participants = RoomParticipantSerializer(many=True, read_only=True)
+    class Meta:
+        model = Room
+        fields = [
+            'id', 'name', 'group', 'created_by', 'created_at',
+            'code', 'participants', 'is_active'
+        ]
+        read_only_fields = ['code', 'created_by', 'created_at', 'participants']
