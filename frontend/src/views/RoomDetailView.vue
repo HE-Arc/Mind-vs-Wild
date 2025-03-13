a2046987dd2c19d029a699716bd1e3d93035ef35
Magnin Nicolas <nicolas.magnin@he-arc.ch>
2025-03-13 11:25:29 +0100

    Fix room detail view

--- frontend/src/views/RoomDetailView.vue
+++ frontend/src/views/RoomDetailView.vue
@@ -1,32 +1,39 @@
 <template>
-  <div class="q-pa-md">
-    <h2>{{ room.name }}</h2>
-    <p>Code de la Room : {{ room.code }}</p>
+  <q-page class="q-pa-md">
+    <div v-if="room">
+      <div class="text-center q-mb-md">
+        <h2 class="text-h5 text-white text-bold q-ma-none">{{ room.name }}</h2>
+        <div class="text-subtitle2 text-grey-7">Code: {{ room.code }}</div>
+      </div>
+      <h3>Participants ({{ room.participants.length }})</h3>
+      <ul>
+        <li v-for="p in room.participants" :key="p.id">{{ p.user.username }}</li>
+      </ul>
+  
+      <q-btn color="negative" label="Quitter la Room" class="q-mt-md" @click="leaveRoom" />
+    </div>
 
-    <h3>Participants ({{ room.participants.length }})</h3>
-    <ul>
-      <li v-for="p in room.participants" :key="p.id">{{ p.username }}</li>
-    </ul>
-
-    <q-btn color="negative" label="Quitter la Room" class="q-mt-md" @click="leaveRoom" />
-  </div>
+    <div v-else class="text-center q-mt-md">
+      <q-spinner size="50px" color="primary" />
+      <p>Loading room details...</p>
+    </div>
+  </q-page>
 </template>
 
 <script setup>
-import { onMounted, computed } from 'vue'
+import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import { useRoomStore } from '@/stores/room'
-import { useRoute, useRouter } from 'vue-router'
 
-const roomStore = useRoomStore()
 const route = useRoute()
-const router = useRouter()
+const roomStore = useRoomStore()
 
-onMounted(() => {
+const room = ref(null)
+
+onMounted(async () => {
   const code = route.params.code
-  roomStore.fetchRoomDetails(code)
+  room.value = await roomStore.fetchRoomDetails(code)
 })
-
-const room = computed(() => roomStore.currentRoom)
 
 // TODO: Implement leaveRoom function
 // const leaveRoom = () => {
